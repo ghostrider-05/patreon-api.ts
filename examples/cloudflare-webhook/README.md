7712a6e159f3bbe84b033135a6cbad8e30996e0a
ghostrider-05 <lucas.duijn@hotmail.nl>
2024-05-09 18:50:43 +0200

    chore: add cloudflare webhook worker

---
+++ examples/cloudflare-webhook/README.md
@@ -0,0 +1,1 @@
+This is an example for how to connect `patreon-api.ts` with Cloudflare Workers to send Discord messages on a new post.
---
+++ examples/cloudflare-webhook/src/index.ts
@@ -0,0 +1,54 @@
+import {
+    PatreonWebhookTrigger,
+    WebhookToDiscordMessages,
+    parseWebhookRequest,
+    webhookToDiscordEmbed,
+} from '../../../src'
+
+interface EnvWithSecrets extends Env {
+    DISCORD_WEBHOOK_URL: string
+    PATREON_WEBHOOK_SECRET: string
+}
+
+export default <ExportedHandler<EnvWithSecrets>> {
+    async fetch (request, env) {
+        if (request.method !== 'POST') {
+            return new Response(JSON.stringify('Invalid method'), { status: 400 })
+        }
+
+        const result = await parseWebhookRequest<PatreonWebhookTrigger.PostPublished>(request, env.PATREON_WEBHOOK_SECRET)
+        if (result.verified !== true) {
+            return new Response('Invalid signature', { status: 403 })
+        }
+
+        const { event, payload } = result
+
+        const options: WebhookToDiscordMessages = {
+            [PatreonWebhookTrigger.PostPublished]: {
+                color: 0x5865F2,
+                title: 'New post: {title}',
+                addContextKeys(payload) {
+                    return {
+                        title: payload.data.attributes.title,
+                    }
+                },
+                extends(payload) {
+                    return {
+                        description: payload.data.attributes.content,
+                    }
+                },
+            }
+        }
+
+        const embed = webhookToDiscordEmbed(event, payload, options)
+        if (embed) {
+            return await fetch(env.DISCORD_WEBHOOK_URL, {
+                method: 'POST',
+                body: JSON.stringify({ embeds: [embed] }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+        }
+    }
+}
