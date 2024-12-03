ae7acfe87fb97096c38bf096a4efe9e7e9772040
ghostrider-05 <lucas.duijn@hotmail.nl>
2024-12-03 16:30:32 +0100

    add related links to types

--- src/schemas/v2/relationships.ts
+++ src/schemas/v2/relationships.ts
@@ -10,108 +10,104 @@ import {
 
 type Multiple<
     IncludedKey extends string,
-    Related extends boolean,
     Appendix extends string = 's'
-> = [`${IncludedKey}${Appendix}`, true, Related]
+> = [`${IncludedKey}${Appendix}`, true]
 
-// TODO: correct related relations
 interface RelationshipTypeMap extends Record<ItemType, BaseRelationDataItem<ItemType>> {
     address: {
-        [Type.Campaign]: Multiple<Type.Campaign, false>
-        [Type.User]: [false, false]
+        [Type.Campaign]: Multiple<Type.Campaign>
+        [Type.User]: [false]
     }
     benefit: {
-        [Type.Campaign]: [true, false]
+        [Type.Campaign]: [true]
         // TODO: add `campaign_installation` when documentated
-        [Type.Deliverable]: [true, false]
-        [Type.Tier]: [true, false]
+        [Type.Deliverable]: [true]
+        [Type.Tier]: [true]
     }
     campaign: {
-        [Type.Benefit]: Multiple<Type.Benefit, false>
+        [Type.Benefit]: Multiple<Type.Benefit>
         // TODO: add `campaign_installation` when documentated
         // TODO: add `categories` when documentated
-        [Type.User]: ['creator', false, false]
-        [Type.Goal]: Multiple<Type.Goal, false>
-        [Type.Tier]: Multiple<Type.Tier, false>
+        [Type.User]: ['creator', false]
+        [Type.Goal]: Multiple<Type.Goal>
+        [Type.Tier]: Multiple<Type.Tier>
     }
     client: {
         // TODO: add `apps` when documented
-        [Type.Campaign]: [false, false]
+        [Type.Campaign]: [false]
         // TODO: add `creator_token` when documented
-        [Type.User]: [false, false]
+        [Type.User]: [false]
     }
     deliverable: {
-        [Type.Benefit]: [false, false]
-        [Type.Campaign]: [false, false]
-        [Type.Member]: [false, false]
-        [Type.User]: [false, false]
+        [Type.Benefit]: [false]
+        [Type.Campaign]: [false]
+        [Type.Member]: [false]
+        [Type.User]: [false]
     }
     /** @deprecated */
     goal: {
-        [Type.Campaign]: [false, false]
+        [Type.Campaign]: [false]
     }
     // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     media: {
         // Nothing
     }
     member: {
-        [Type.Address]: [false, false]
-        [Type.Campaign]: [false, false]
-        [Type.Tier]: ['currently_entitled_tiers', true, false]
-        [Type.PledgeEvent]: ['pledge_history', true, false]
-        [Type.User]: [false, false]
+        [Type.Address]: [false]
+        [Type.Campaign]: [false]
+        [Type.Tier]: ['currently_entitled_tiers', true]
+        [Type.PledgeEvent]: ['pledge_history', true]
+        [Type.User]: [false]
     }
     'pledge-event': {
-        [Type.Campaign]: [false, false]
-        [Type.User]: ['patron', false, false]
-        [Type.Tier]: [false, false]
+        [Type.Campaign]: [false]
+        [Type.User]: ['patron', false]
+        [Type.Tier]: [false]
     }
     post: {
-        [Type.User]: [false, false]
-        [Type.Campaign]: [false, false]
+        [Type.User]: [false]
+        [Type.Campaign]: [false]
     }
     tier: {
-        [Type.Benefit]: Multiple<Type.Benefit, false>
-        [Type.Campaign]: [false, false]
-        [Type.Media]: ['tier_image', false, false]
+        [Type.Benefit]: Multiple<Type.Benefit>
+        [Type.Campaign]: [false]
+        [Type.Media]: ['tier_image', false]
     }
     user: {
-        [Type.Campaign]: [false, false]
-        [Type.Member]: ['memberships', true, false]
+        [Type.Campaign]: [false]
+        [Type.Member]: ['memberships', true]
     }
     webhook: {
-        [Type.Campaign]: [false, false]
-        [Type.Client]: [false, false]
+        [Type.Campaign]: [false]
+        [Type.Client]: [false]
     }
 }
 
 type ResolvedRelation<T extends `${Type}`> = {
     [R in keyof RelationshipTypeMap[T]]: R extends string
-        ? RelationshipTypeMap[T][R] extends [boolean, boolean]
-            ? [R, RelationshipTypeMap[T][R][0] extends true ? `${R}s` : `${R}`, RelationshipTypeMap[T][R][0], RelationshipTypeMap[T][R][1]]
-            : RelationshipTypeMap[T][R] extends [string, boolean, boolean]
-                ? [R, RelationshipTypeMap[T][R][0], RelationshipTypeMap[T][R][1], RelationshipTypeMap[T][R][2]]
+        ? RelationshipTypeMap[T][R] extends [boolean]
+            ? [R, RelationshipTypeMap[T][R][0] extends true ? `${R}s` : `${R}`, RelationshipTypeMap[T][R][0]]
+            : RelationshipTypeMap[T][R] extends [string, boolean]
+                ? [R, RelationshipTypeMap[T][R][0], RelationshipTypeMap[T][R][1]]
                 : never
             : never
 }
 
-type BaseRelationDataItem<T extends string> = Partial<Record<T, [string, boolean, boolean] | [boolean, boolean]>>
+type BaseRelationDataItem<T extends string> = Partial<Record<T, [string, boolean] | [boolean]>>
 type RelationStringIndex =
     | 0
     | 1
 
 type RelationDataItem<RelationType extends Type | `${Type}`, Index extends RelationStringIndex> =
-    ResolvedRelation<`${RelationType}`> extends Partial<Record<Type, [Type, string, boolean, boolean]>>
+    ResolvedRelation<`${RelationType}`> extends Partial<Record<Type, [Type, string, boolean]>>
     ? {
         [Key in ResolvedRelation<RelationType>[keyof ResolvedRelation<RelationType>][Index]]:
             ResolvedRelation<RelationType>[keyof ResolvedRelation<RelationType>] extends infer Relation
-                // ? Relation
-                ? Relation extends [Type, string, boolean, boolean]
-                    // ? R : never
+                ? Relation extends [Type, string, boolean]
                     ? Key extends Relation[Index]
                         ? Relation[2] extends true
-                            ? DataItems<Relation[0], Relation[3]>
-                            : DataItem<Relation[0], Relation[3]>
+                            ? DataItems<Relation[0], false>
+                            : DataItem<Relation[0], true>
                         : never
                     : never
                 : never
