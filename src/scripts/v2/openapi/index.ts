1e28615eb55cd92a6ae9390823fabaef429eef72
ghostrider-05 <lucas.duijn@hotmail.nl>
2024-12-06 22:17:43 +0100

    ci: update spec integration

--- src/scripts/v2/openapi/index.ts
+++ src/scripts/v2/openapi/index.ts
@@ -6,7 +6,11 @@ import writeSchema from './api'
     const folder = process.env['OPENAPI_FOLDER'] ?? './tsc-generated/openapi/'
     if (!folder) throw new Error()
 
-    await mkdir(folder)
+    try {
+        await mkdir(folder)
+    } catch {
+        // Dir already exists or something is really wrong
+    }
 
     await writeSchema(folder + 'openapi.json', false)
     await writeSchema(folder + 'openapi_preview.json', true)
