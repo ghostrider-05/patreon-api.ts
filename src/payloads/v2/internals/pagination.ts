1140d5e37d6416042741dfe3a7deec614cbc5f20
ghostrider-05 <lucas.duijn@hotmail.nl>
2025-03-18 00:03:49 +0100

    fix(types): next cursor is nullable

--- src/payloads/v2/internals/pagination.ts
+++ src/payloads/v2/internals/pagination.ts
@@ -1,6 +1,9 @@
 export interface ListRequestPaginationPayload {
     total: number
+    // This can be null according to the JSON:API (I think)
+    // But it is always returned from my observation, so I don't think I know if it should be optional
+    // Leaving it now for how it was before.
     cursors?: {
-        next: string
+        next: string | null
     }
 }
