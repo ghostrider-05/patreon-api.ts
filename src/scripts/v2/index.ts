711f6315abc2d5a00516cebd39b89b5be561836c
ghostrider-05 <lucas.duijn@hotmail.nl>
2025-04-24 23:36:40 +0200

    feat!: remove deprecated SchemaRelationshipKeys and SchemaKeys

--- src/scripts/v2/index.ts
+++ src/scripts/v2/index.ts
@@ -1,12 +1,8 @@
-import { syncResourceKeys } from './keys'
-import { syncRelationships } from './relationships'
 import { syncRandomData } from './random'
 import { syncResourceSchemas } from './schemas'
 
 // eslint-disable-next-line jsdoc/require-jsdoc
 async function syncSchemas () {
-    await syncResourceKeys()
-    await syncRelationships()
     await syncResourceSchemas()
 
     await syncRandomData()
