81e38e4fb7134de2078d8829f68c07c975dc44e8
ghostrider-05 <lucas.duijn@hotmail.nl>
2025-05-27 17:41:38 +0200

    deprecated RouteBases.api

--- src/rest/v2/routes.ts
+++ src/rest/v2/routes.ts
@@ -1,6 +1,7 @@
 import { APIVersion } from './version'
 
 export const RouteBases = {
+    /** @deprecated This is not used in the API documentation */
     api: `https://patreon.com/api/v${APIVersion}`,
     oauth2: `https://patreon.com/api/oauth2/v${APIVersion}`,
 } as const
